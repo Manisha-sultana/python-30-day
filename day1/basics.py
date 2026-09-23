77a4d5e785d3c82875877c895c796f6d249bfe52
= <=>
2026-09-23 16:33:12 +0530

    Complete Day 1 Python programs

---
+++ day1/basics.py
@@ -0,0 +1,10 @@
+# This is my first Python comment
+
+name = "Manisha"
+age = 21
+
+if age >= 18:
+    print("You are an adult.")
+    print("Your name is", name)
+else:
+    print("You are below 18.")
