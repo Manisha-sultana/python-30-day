fb4255ec41b0c2a6de5ff682e6350f4c48ca026c
= <=>
2026-09-23 20:05:43 +0530

    Add Python data types

---
+++ day2/data_type.py
@@ -0,0 +1,17 @@
+name = "Manisha"
+age = 21
+height = 5.4
+is_student = True
+result = None
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(result)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(result))
