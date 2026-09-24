1edf35389259c619d9c8432d01266eea45baae5c
= <=>
2026-09-24 12:12:58 +0530

    Add unit conversion programs

--- day2/units.py
+++ day2/units.py
@@ -1,5 +1,14 @@
+#Kilometares to Metres
 kilometres = float(input("Enter distance in kilometres: "))
 
 metres = kilometres * 1000
 
 print(f"Distance in metres: {metres}")
+
+# Celsius to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(f"Temperature in Fahrenheit: {fahrenheit}")
