82f5cb65bf874d8254b4515d32e0ac3c093a0cef
= <=>
2026-09-24 15:22:37 +0530

    init

---
+++ day3/string_basics.py
@@ -0,0 +1,18 @@
+name = "Manisha Sultana"
+
+print(name)
+print(len(name))
+
+# Indexing
+print(name[0])
+print(name[1])
+
+# Slicing
+print(name[0:7])
+print(name[:7])
+print(name[8:])
+
+# String methods
+print(name.upper())
+print(name.lower())
+print(name.title())
