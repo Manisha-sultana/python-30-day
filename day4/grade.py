68bde2ce6caa44cc65e7d272f2eae29d1b836b25
= <=>
2026-09-25 15:28:35 +0530

    init

---
+++ day4/grade.py
@@ -0,0 +1,13 @@
+marks = float(input("Enter your marks: "))xz
+if marks < 0 or marks > 100:
+    print("Invalid marks. Please enter marks between 0 and 100.")
+elif marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
